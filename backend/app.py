4e2653f64c2d6ac86733cc35f3c2f7bf40670af9
nathan <felicianathanael52@gmail.com>
2024-11-06 10:37:16 +0800

    host

--- backend/app.py
+++ backend/app.py
@@ -48,5 +48,5 @@ async def read_users_me(current_user=Depends(get_current_user)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="172.22.30.112", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
