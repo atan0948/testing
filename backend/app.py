18f7a19dda83363f8d842a5a7abd26264d45e2fd
nathan <felicianathanael52@gmail.com>
2024-11-25 16:39:23 +0800

    cors

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ origins = [
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
